d817794f434b081fa71e2b8cd512406ff66d3a4c
Leonardo <kibiluzbad@hotmail.com>
2011-10-11 21:19:11 -0300

    Adding user.

---
+++ user.rb
@@ -0,0 +1,9 @@
+class User < Picasa::DefaultUser
+  def picasa_id
+    "moviescatalogonrails"
+  end
+  
+  def auth_token
+    "DQAAALwAAAB6jXxSXE6ohv7btQq5-XurlMAzpmd4DL9HWIxXvSnL3fOlPIcGyl8ELldUplG9xPdKwu-eay_dCmZCnypgKUSMs9bKBZouT7lAT2eTLr_KcoVrQPz7heBDbsa_JOdjDG3nZCEwWUIW0nKmkcfWQhtulu4cjBOPvRZcssLDBYUZh503geDZNARKXhbZtxzH4jrkRRqCAUlPwz7upxgh8oyZYOAUcIm2UNcXbRIfEROUaEAdyLI-k6z9CaDagQ9l0kk"
+  end
+end
